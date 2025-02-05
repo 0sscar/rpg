dce544eef42ea84717a8b1f19a3d0fcabcffa943
0sscar <gosg.gabriel@hotmail.com>
2025-02-05 13:57:21 -0300

    Update main.py

--- text_rpg/main.py
+++ text_rpg/main.py
@@ -1,10 +1,30 @@
 class Player: 
 
-    def __init__(self, age):  
-        self.age = age 
-           
-    def get_age (self): 
-        return self.age
+    def __init__(self, helth = int, name= str, damage= int ):  
+        self.name = name 
+        self.helth = helth
+        #self.max_hp = max_hp 
+          
+        self.damage = damage 
 
-a = Player(17)  
-print (a.get_age())  
+    def atack (self, target): 
+        target.helth -= self.damage 
+        target.helth = max(target.helth, 0) 
+
+    def get_helth(self):        
+    
+        return(self.helth)
+
+Hero = Player(helth =100, name = "Hero", damage=35 ) 
+Villan = Player(helth=100, name= "Villan", damage=15) 
+
+
+while True: 
+    Hero.damage(Villan) 
+    Villan.damage(Hero) 
+
+    print(f"Name: {Hero.name} HP:{Hero.helth}") 
+    print(f"Name: {Villan.name} HP:{Villan.helth}") 
+    
+    input() 
+ 
